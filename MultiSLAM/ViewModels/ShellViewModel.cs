ffa1bfd697421fe146d0c983447521639da34738
Jonas Wallat <jwallat@hotmail.de>
2017-08-22 17:45:45 +0200

    init + caliburn micro

---
+++ MultiSLAM/ViewModels/ShellViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace MultiSLAM.ViewModels
+{
+    class ShellViewModel : Screen
+    {
+
+        private string _firstName = "Jonas";
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value; 
+                NotifyOfPropertyChange(() => FirstName);
+            }
+        }
+ 
+
+    }
+}
